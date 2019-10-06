d9fe91fe938fac705b27169dec6b0f63c824651d
sompodsign <47793716+sompodsign@users.noreply.github.com>
2019-10-07 02:27:32 +0600

    removed gitignore

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postsSchema = {
+  title: String,
+  content: String
+};
+const Post = mongoose.model("Post", postsSchema);
+
+const homeStartingContent = "This is Shampad Sharkar. I'm from Bangladesh. I was born and raised in Sherpur but moved here(Dhaka) 5 years ago for my further study. Now I'm working as a freelance web developer. Thank you for visiting my blog.";
+const aboutContent = "Thank you so much for visiting my journal. I always dreamed about my own journal where I'll keep notes for everything where I'll be able to share my daily stories. So here it is. I am Shampad Sharkar. Currently I'm living in Dhaka, Bangladesh and working as a freelance web developer. When I dreamed about my personal journal I didn't know how to build one. I think now I'll be able to keep my daily journal here. ";
+const contactContent = "Email: sompod123@gmail.com";
 
 const app = express();
 
@@ -20,14 +29,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-const posts = [];
-
 app.get("/", function(req, res) {
-  res.render("home", {
-    postOne: homeStartingContent,
-    posts: posts
+  Post.find({}, function(err, posts) {
+    res.render("home", {
+      postOne: homeStartingContent,
+      posts: posts
+    });
   });
 });
+
 
 app.get("/about", function(req, res) {
   res.render("about", {
@@ -46,33 +56,23 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postText
-  };
-
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
-
-
 });
 
-app.get("/posts/:sompod", function(req, res) {
-  const paramName = _.lowerCase(req.params.sompod);
-
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (paramName === storedTitle) {
-      res.render("post", {
+app.get("/posts/:postId", function(req, res) {
+const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post) {
+      res.render("post",  {
         postTitle: post.title,
         postContent: post.content
       });
-    }
   });
-
 });
-
 
 
 app.listen(3000, function() {
